09fa283202dfebac50e793875ab89051c8084856
fly <devfly@flytech.video>
2024-12-02 19:16:06 +0100

    Complete Day 1

--- src/day1/Day1.kt
+++ src/day1/Day1.kt
@@ -1,14 +1,34 @@
 package day1
 
+import kotlin.math.abs
+
 class Day1 {
 
-    private val input = InputReader.readInput(1, test = true)
+    private val input = InputReader.readInput(1, test = false)
 
     fun solve1() {
-
+        println(
+            input.map { row ->
+                row.split("   ")
+                    .map { it.toInt() }
+                    .let{ it[0] to it[1] }
+            }.unzip()
+            .toList()
+            .map { it.sorted() }
+            .let { it[0].zip(it[1]) }
+            .fold(0) { acc, pair -> acc + abs(pair.second - pair.first) }
+        )
     }
 
     fun solve2() {
-
+        val lists = input.map { row ->
+            row.split("   ")
+                .map { it.toInt() }
+                .let{ it[0] to it[1] }
+        }.unzip()
+        val frequencies = lists.second.groupingBy { it }.eachCount()
+        println(
+            lists.first.sumOf { frequencies.getOrDefault(it, 0) * it }
+        )
     }
 }
