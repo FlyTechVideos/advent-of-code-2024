1d9b15b1cb131595966d7b3cc5583a23640dccb5
fly <devfly@flytech.video>
2024-12-09 21:27:21 +0100

    Complete Day 9 Part 2

--- src/day9/Day9.kt
+++ src/day9/Day9.kt
@@ -4,7 +4,7 @@ import InputReader
 
 class Day9 {
 
-    private val input = InputReader.readInput(9, test = true, split = false)[0]
+    private val input = InputReader.readInput(9, test = false, split = false)[0]
 
     fun solve1() {
         val numbers = input.split("").drop(1).dropLast(1).map { it.toInt() }.toMutableList()
@@ -43,37 +43,54 @@ class Day9 {
 
         var computedPosition = 0L
 
-        while (startPointer <= endPointer) {
-            val numbersDebug = numbers.joinToString("")
+        val additionalEmptySpaces = mutableSetOf<Int>()
 
-            if (startPointer % 2 == 0) {
+        while (startPointer <= numbers.size - 1) {
+            if (startPointer % 2 == 0 && !additionalEmptySpaces.contains(startPointer)) {
                 checksum += (startPointer / 2) * computedPosition
                 numbers[startPointer]--
+                computedPosition++
             } else {
+                if (endPointer == 0) {
+                    computedPosition += numbers[startPointer]
+                    startPointer++
+                    continue
+                }
                 val freeSpaceSize = numbers[startPointer]
                 var tempEndPointer = endPointer
 
-                while (numbers[tempEndPointer] > freeSpaceSize || numbers[tempEndPointer] == 0) {
+                while (
+                    tempEndPointer > startPointer &&
+                    (
+                            numbers[tempEndPointer] > freeSpaceSize ||
+                            numbers[tempEndPointer] == 0 ||
+                            additionalEmptySpaces.contains(tempEndPointer)
+                    )
+                ) {
                     tempEndPointer -= 2
-                    if (tempEndPointer < 0) {
-                        println("what")
-                    }
                 }
 
-                while (numbers[tempEndPointer] > 0) {
-                    checksum += (tempEndPointer / 2) * computedPosition
-                    computedPosition++
-                    numbers[startPointer]--
-                    numbers[tempEndPointer]--
+                if (tempEndPointer <= startPointer) {
+                    computedPosition += freeSpaceSize
+                    numbers[startPointer] = 0
+                    startPointer++
+                } else {
+                    var movedFileSize = numbers[tempEndPointer]
+                    while (movedFileSize > 0) {
+                        checksum += (tempEndPointer / 2) * computedPosition
+                        computedPosition++
+                        numbers[startPointer]--
+                        movedFileSize--
+                    }
+                    additionalEmptySpaces.add(tempEndPointer)
                 }
             }
-            while (numbers[startPointer] == 0 && startPointer < numbers.size - 1) {
+            while (startPointer < numbers.size && numbers[startPointer] == 0) {
                 startPointer++
             }
             while (numbers[endPointer] == 0 && endPointer > 1) {
                 endPointer -= 2
             }
-            computedPosition++
         }
 
         println(checksum)
